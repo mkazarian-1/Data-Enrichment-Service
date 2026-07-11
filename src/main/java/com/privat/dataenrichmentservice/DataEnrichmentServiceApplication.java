0da4feedcb82c169da950186b4c09caac778f239
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2026-07-11 17:24:03 +0300

    Initial commit

---
+++ src/main/java/com/privat/dataenrichmentservice/DataEnrichmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.privat.dataenrichmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEnrichmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataEnrichmentServiceApplication.class, args);
+    }
+
+}
